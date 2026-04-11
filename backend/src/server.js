e276db9aa647b8abe1991dec60bbe57a0a363ab2
Shruti Singh <821shrutisingh@gmail.com>
2026-04-12 02:10:04 +0530

    Initial commit: Set up full frontend and foundational 30% of backend

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("?? Error starting the server", error);
+  }
+};
+
+startServer();
